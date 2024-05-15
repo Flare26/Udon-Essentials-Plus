585fefd68634119d48501c7a531da0bdabb97a3f
Nathan <nateboy57@yahoo.com>
2024-05-14 22:51:06 -0500

    Support for custom events exit disabler

--- n8bits Udon Essentials+/TriggerExitDisabler.cs
+++ n8bits Udon Essentials+/TriggerExitDisabler.cs
@@ -1,28 +1,52 @@
-﻿// Trigger Exit Disabler by N8bits
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
 namespace UEPlus
 {
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None )]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class TriggerExitDisabler : UdonSharpBehaviour
     {
         [Header("Trigger Exit Disabler by n8")]
-        [Header("v1.0.0")]
-        [SerializeField] GameObject[] disables; 
+        [Header("v1.0.1")]
+        [SerializeField] GameObject[] disables;
+        [Header("Alternative method")]
+        [Tooltip("Fires interact() event on behaviours array instead of using disables array."), SerializeField] bool useEvents;
+        [SerializeField] string methodName;
+        [Tooltip("Will fire the Interact() event on these objects"), SerializeField] UdonBehaviour[] behaviours;
+
         public override void OnPlayerTriggerExit(VRCPlayerApi api)
         {
+            Debug.Log("OnPlayerTriggerExit called");
+
             if (Networking.LocalPlayer.Equals(api))
             {
-                foreach ( GameObject o in disables)
+                if (!useEvents)
                 {
-                    if (o.activeSelf == true)
-                        o.SetActive(false);
+                    foreach (GameObject o in disables)
+                    {
+                        if (o.activeSelf == true)
+                        {
+                            o.SetActive(false);
+                            Debug.Log($"Disabling GameObject: {o.name}");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (UdonBehaviour o in behaviours)
+                    {
+                        Debug.Log($"Checking program var on {o.name}");
+                        bool state = (bool) o.GetProgramVariable("state");
+                        if (state)
+                        {
+                            Debug.Log($"State true. Sending custom event on {o.name}");
+                            o.SendCustomEvent(methodName);
+                        }
+                    }
                 }
             }
         }
     }
-
 }
