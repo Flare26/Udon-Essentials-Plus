0f4f2ef016d2cda7b4132962189a52921fb0a725
Nathan <nateboy57@yahoo.com>
2024-06-20 20:41:30 -0500

    Added Podcast Camera System

---
+++ Cam Switcher/TripodLevel.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class TripodLevel : UdonSharpBehaviour
+{
+    [SerializeField] Transform tripodParent;
+    void Start()
+    {
+        
+    }
+
+    public void LevelTripod()
+    {
+        if (!Networking.GetOwner(tripodParent.gameObject).Equals(Networking.LocalPlayer))
+            Networking.SetOwner(Networking.LocalPlayer, tripodParent.gameObject);
+
+        Vector3 newAngles = new Vector3(0f, tripodParent.eulerAngles.y, 0f);
+        tripodParent.eulerAngles = newAngles;
+    }
+}
---
+++ n8bits Udon Essentials+/ListManager.cs
@@ -0,0 +1,94 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ListManager : UdonSharpBehaviour
+{
+
+    [SerializeField] string[] blackListed;
+    [SerializeField] string[] masterWhitelist; // admin
+    [SerializeField] string[] djWhitelist;
+    [SerializeField] string[] dancerWhitelist;
+
+    [SerializeField] GameObject[] djObjs;
+    [SerializeField] GameObject[] dancerObjs;
+    [SerializeField] GameObject[] adminObjs;
+
+    [SerializeField] Transform banTP;
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        // if it's not the local player that joined, don't execute any checks otherwise objects will toggle.
+        if (!player.displayName.Equals(Networking.LocalPlayer.displayName))
+            return;
+
+        //Blacklist handling
+        foreach(string name in blackListed)
+        {
+                // check the player who just joined
+            if (Networking.LocalPlayer.displayName.Equals(name,System.StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log("Blackisted Player joined ... " + player);
+                player.TeleportTo(banTP.position, banTP.rotation);
+            }
+        }
+
+        foreach (string name in djWhitelist)
+        {
+            // check the player who just joined
+            if (Networking.LocalPlayer.displayName.Equals(name, System.StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log("Whitelisted DJ joined ... " + player);
+                ToggleDJObjs();
+            }
+        }
+
+        foreach (string name in dancerWhitelist)
+        {
+            // check the player who just joined
+            if (Networking.LocalPlayer.displayName.Equals(name, System.StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log("Whitelisted dancer joined ... " + player);
+                ToggleDancerObjs();
+            }
+        }
+
+        foreach (string name in masterWhitelist)
+        {
+            // check the player who just joined
+            if (Networking.LocalPlayer.displayName.Equals(name, System.StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log("Whitelisted DJ joined ... " + player);
+                ToggleAdminObjs();
+            }
+        }
+    }
+
+    private void ToggleAdminObjs()
+    {
+        ToggleDJObjs();
+        ToggleDancerObjs();
+        foreach (GameObject o in adminObjs)
+        {
+            o.SetActive(!o.activeSelf);
+        }
+    }
+
+    private void ToggleDJObjs()
+    {
+        foreach (GameObject o in djObjs)
+        {
+            o.SetActive(!o.activeSelf);
+        }
+    }
+
+    private void ToggleDancerObjs()
+    {
+        foreach (GameObject o in dancerObjs)
+        {
+            o.SetActive(!o.activeSelf);
+        }
+    }
+}
