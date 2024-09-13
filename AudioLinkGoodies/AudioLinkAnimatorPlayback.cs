0f9e04af66550d63d268e26fc73013d767e66c88
Nathan <nateboy57@yahoo.com>
2024-09-13 08:39:33 -0500

    Audio Link Goodies

---
+++ AudioLinkGoodies/AudioLinkAnimatorPlayback.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+namespace AudioLink
+{
+#if UDONSHARP
+    using UdonSharp;
+
+    public class AudioLinkAnimatorPlayback : UdonSharpBehaviour
+#else
+    public class AudioLinkAnimatorPlayback : MonoBehaviour
+#endif
+    {
+        public AudioLink audioLink;
+        public Animator anim;
+        public int emitCount;
+        public int band;
+        [Range(0, 127)]
+        public int delay;
+        public float sensitivity;
+        public float speedToAudioFactor;
+        public float speedDampening;
+        [Range(0f, 1f)] public float animOffset;  // New animation offset parameter (0 to 1)
+
+        private int _dataIndex;
+        private bool animationStarted = false;  // To track if animation has been started
+
+        void Start()
+        {
+            UpdateDataIndex();
+            anim.speed = 0;  // Start with the animation paused
+
+            // Set the animation start position based on the animOffset
+            SetAnimationOffset(animOffset);
+        }
+
+        void Update()
+        {
+            Color[] audioData = audioLink.audioData;
+            if (audioData.Length != 0)  // Check for audioLink initialization
+            {
+                // Get the amplitude for the current band
+                float amplitude = audioData[_dataIndex].grayscale;
+
+                //Debug.Log("Amplitude = " + amplitude);
+
+                // If amplitude meets the sensitivity threshold, set the animation speed
+                if (amplitude >= sensitivity)
+                {
+                    if (!animationStarted)
+                    {
+                        // Set the animation offset the first time the animation starts
+                        SetAnimationOffset(animOffset);
+                        animationStarted = true;
+                    }
+
+                    anim.speed = amplitude * speedToAudioFactor;
+                }
+                else
+                {
+                    // Smoothly decrease the animation speed based on speed dampening
+                    anim.speed = Mathf.Lerp(anim.speed, 0f, Time.deltaTime * speedDampening);
+                }
+            }
+        }
+
+        // Update the data index based on band and delay
+        public void UpdateDataIndex()
+        {
+            _dataIndex = (band * 128) + delay;
+        }
+
+        // Set the normalized time (offset) for the animation
+        private void SetAnimationOffset(float offset)
+        {
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+            // Play the animation at the specified offset (normalized time)
+            anim.Play(stateInfo.fullPathHash, -1, offset);
+        }
+    }
+}
---
+++ AudioLinkGoodies/AudioLinkParticleEmit.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace AudioLink
+{
+#if UDONSHARP
+    using UdonSharp;
+
+    public class AudioLinkParticleEmit : UdonSharpBehaviour
+#else
+    public class AudioLinkParticleEmit : MonoBehaviour
+#endif
+    {
+        public AudioLink audioLink;
+        public ParticleSystem particleSystem;
+        public int emitCount;
+        public int band;
+        [Range(0, 127)]
+        public int delay;
+        public float sensitivity;
+        public float speedToAudioFactor;
+        //public Vector3 position;
+        //public Vector3 rotation;
+        //public Vector3 scale = new Vector3(1f, 1f, 1f);
+
+
+        private int _dataIndex;
+        //private Vector3 _initialPosition;
+        //private Vector3 _initialRotation;
+        //private Vector3 _initialScale;
+
+        void Start()
+        {
+            UpdateDataIndex();
+            //_initialPosition = transform.localPosition;
+            //_initialRotation = transform.localEulerAngles;
+            //_initialScale = transform.localScale;
+
+        }
+
+        void Update()
+        {
+            Color[] audioData = audioLink.audioData;
+            if (audioData.Length != 0)      // check for audioLink initialization
+            {
+                float amplitude = audioData[_dataIndex].grayscale;
+
+                //Debug.Log("Amplitude = " + amplitude);
+                if (amplitude >= sensitivity)
+                {
+                    var velocityModule = particleSystem.velocityOverLifetime;
+                    velocityModule.speedModifier = 1 + (speedToAudioFactor * amplitude);
+                    particleSystem.Emit(emitCount);
+                }
+                //transform.localPosition = _initialPosition + (position * amplitude);
+                //transform.localEulerAngles = _initialRotation + (rotation * amplitude);
+
+                //transform.localScale = new Vector3(_initialScale.x * Mathf.Lerp(1f, scale.x, amplitude), _initialScale.y * Mathf.Lerp(1f, scale.y, amplitude), _initialScale.z * Mathf.Lerp(1f, scale.z, amplitude));
+            }
+        }
+
+        public void UpdateDataIndex()
+        {
+            _dataIndex = (band * 128) + delay;
+        }
+    }
+}
